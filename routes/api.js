89003847ef28b19b416eb14e8f631fb1310f663b
Guuzzeji <5lh8dl440f@gmail.com>
2023-03-29 00:11:51 -0700

    Putting in stub for API

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
-const router = express.router();
+const router = express.Router();
 
 router.get('/', (req, res) => {
-
+    res.send("API");
 });
 
 module.exports = router;
