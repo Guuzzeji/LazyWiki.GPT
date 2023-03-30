756e3f70272c4fe0e811db54f4d7182acec032db
Guuzzeji <5lh8dl440f@gmail.com>
2023-03-29 21:15:19 -0700

    Creating prompts

---
+++ GPT/prompt.mjs
@@ -0,0 +1,38 @@
+import { PromptTemplate, SystemMessagePromptTemplate } from "langchain/prompts";
+import { StructuredOutputParser } from "langchain/output_parsers";
+
+import { removeStopWords } from "./stopWord.js";
+
+const WikipageStrucutre = StructuredOutputParser.fromNamesAndDescriptions({
+    answer: "sources used to answer the user's question, should be a Wikipedia website url.",
+});
+
+const answerQuestion = StructuredOutputParser.fromNamesAndDescriptions({
+    answer: "answer to the user's question with annotation for Wikipedia websites used. Example: some text {1}",
+    source: [
+        "sources used to answer the user's question, should be a Wikipedia website url.",
+    ]
+});
+
+const promptWikiPages = new PromptTemplate({
+    template:
+        "Answer the users question using Wikipedia website' \n{format_instructions}\n{question}",
+    inputVariables: ["question"],
+    partialVariables: { format_instructions: answerQuestion.getFormatInstructions() },
+});
+
+
+
+
+async function main() {
+
+    const input = await promptWikiPages.format({
+        question: "Describe the vslam",
+    });
+
+    console.log(JSON.stringify(input));
+
+    console.log(removeStopWords("Describe the vslam"));
+}
+
+main();
