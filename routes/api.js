8e127f9d1cba067c995e12c4baefc1b0a708c97c
Guuzzeji <5lh8dl440f@gmail.com>
2023-03-31 00:26:52 -0700

    Creating rate limiting for api request

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,10 @@
 import express from 'express';
 
+import requestLimter from './request-limiter.js';
+
 const router = express.Router();
 
-router.get('/question?=:qs', (req, res) => {
+router.get('/question=:qs', requestLimter, (req, res) => {
     // TODO connect to openai api in gpt folder
     res.send(req.params.qs);
 });
---
+++ routes/request-limiter.js
@@ -0,0 +1,14 @@
+const requests = {};
+
+export default function requestLimter(req, res, next) {
+    const ip = req.ip;
+    const now = Date.now();
+    const timeFrame = 5 * 60 * 1000; // 5 minutes
+
+    if (requests[ip] && requests[ip] > now - timeFrame) {
+        return res.status(429).json({ message: 'Too many requests' });
+    }
+
+    requests[ip] = now;
+    next();
+}
