0fb89e852b0fca0239989c4e912afd351b6e3215
Guuzzeji <5lh8dl440f@gmail.com>
2023-03-30 23:17:53 -0700

    Creating a better prompt message for GPT

--- GPT/prompt.js
+++ GPT/prompt.js
@@ -1,27 +1,26 @@
-import { PromptTemplate, SystemMessagePromptTemplate } from "langchain/prompts";
+import { PromptTemplate } from "langchain/prompts";
 import { StructuredOutputParser } from "langchain/output_parsers";
 
+import searchWiki from "../fetch/duckduckgo.js";
+
 const answerUserStruct = StructuredOutputParser.fromNamesAndDescriptions({
-    answers: `The answer to the user's question with citations of the 
-    source used to answer the question. Example of a citations: some text [1](the website url).`,
-    best: "The website that will most likely answer the user's question",
-    sources: ["Other websites that could also answer the user's question'"],
+    answers: "The answer to the user's question with citations of the source used to answer the question. Example of a citations: some text[^1](the website url).",
+    best: "Should be a website url will most likely answer the user's question",
+    sources: ["Should be a list of websites urls that were used to answer the user's question'"],
 });
 
-
 const promptTempQS = new PromptTemplate({
-    template: `You are a AI name GPT, your job is to answer user questions using both your own knowledge
-    of the world and the infomations of websites that are given to you in a json format. 
-    Answer the user questions to the best of your ability. Keep your answer to the user question within 500 words.
-    \n{format_instructions} \n{question} \n{websites-json-infomation}`,
+    template: "You are a AI name GPT and you should always write your answers in json, your job is to answer user questions using both your own knowledge and the information give to you by websites that are in a json format. Answer the user questions to the best of your ability. Keep your answer to the user question within 200 words. User question: {question}\n List of Websites in json: {websites-json-infomation}\n {format_instructions}",
     inputVariables: ["question", "websites-json-infomation"],
     partialVariables: { format_instructions: answerUserStruct.getFormatInstructions() },
 });
 
 async function main() {
+    let links = await searchWiki("what is vslam");
 
     const input = await promptTempQS.format({
-        question: "Describe the vslam",
+        question: "what is vslam",
+        "websites-json-infomation": JSON.stringify(links)
     });
 
     console.log(JSON.stringify(input));
