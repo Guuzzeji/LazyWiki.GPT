005ecb4fd403bbc7fd15536f3ffb5cee65eb5ee1
Guuzzeji <5lh8dl440f@gmail.com>
2023-03-31 00:03:50 -0700

    Install openai api and created func to use api

---
+++ GPT/index.js
@@ -0,0 +1,27 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+    apiKey: "place holder",
+});
+
+const modelType = "";
+
+const openai = new OpenAIApi(configuration);
+
+// Look at https://github.com/openai/openai-node/issues/107
+export default async function (promptStr) {
+    const completion = await openai.createCompletion({
+        "model": modelType,
+        "prompt": promptStr,
+        "max_tokens": 100,
+        "temperature": 0,
+        "top_p": 1,
+        "n": 1,
+        "stream": true,
+        "logprobs": null,
+        "stop": "\n"
+    }, { responseType: "stream" });
+
+    return completion;
+}
+
