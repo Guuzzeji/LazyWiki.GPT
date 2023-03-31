7413d3e5584ee253f31b9a87021f4ad01a1667c6
Guuzzeji <5lh8dl440f@gmail.com>
2023-03-30 21:33:32 -0700

    Fix prompt structure to fit within one prompt

--- GPT/prompt.mjs
+++ GPT/prompt.mjs
@@ -2,34 +2,29 @@ import { PromptTemplate, SystemMessagePromptTemplate } from "langchain/prompts";
 import { StructuredOutputParser } from "langchain/output_parsers";
 
 const answerUserStruct = StructuredOutputParser.fromNamesAndDescriptions({
-    answers: "The answer to the user's question with citations of the source used to answer the question. Example of a citations: some text [1](the website url).",
-});
-
-const identifyWebsitesStruct = StructuredOutputParser.fromNamesAndDescriptions({
-    top: "The website that will most likely answer the user's question",
+    answers: `The answer to the user's question with citations of the 
+    source used to answer the question. Example of a citations: some text [1](the website url).`,
+    best: "The website that will most likely answer the user's question",
     sources: ["Other websites that could also answer the user's question'"],
 });
 
 
-const promptWikiPages = new PromptTemplate({
-    template:
-        "Answer the users question using Wikipedia website' \n{format_instructions}\n{question}",
-    inputVariables: ["question"],
-    partialVariables: { format_instructions: answerQuestion.getFormatInstructions() },
+const promptTempQS = new PromptTemplate({
+    template: `You are a AI name GPT, your job is to answer user questions using both your own knowledge
+    of the world and the infomations of websites that are given to you in a json format. 
+    Answer the user questions to the best of your ability. Keep your answer to the user question within 500 words.
+    \n{format_instructions} \n{question} \n{websites-json-infomation}`,
+    inputVariables: ["question", "websites-json-infomation"],
+    partialVariables: { format_instructions: answerUserStruct.getFormatInstructions() },
 });
-
-
-
 
 async function main() {
 
-    const input = await promptWikiPages.format({
+    const input = await promptTempQS.format({
         question: "Describe the vslam",
     });
 
     console.log(JSON.stringify(input));
-
-    console.log(removeStopWords("Describe the vslam"));
 }
 
 main();
