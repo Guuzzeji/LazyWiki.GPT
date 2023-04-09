a04005facc9719931a49f952ac2a69865055b763
Guuzzeji <5lh8dl440f@gmail.com>
2023-04-09 00:50:17 -0700

    Connecting to openai

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,19 @@
 import express from 'express';
 
-import createPrompt from './GPT/prompt.js';
+import createPrompt from '../GPT/prompt.js';
 import openaiApi from '../GPT/openai-api.js';
 
 import requestLimter from './request-limiter.js';
 
 const router = express.Router();
 
-router.get('/question=:qs', requestLimter, (req, res) => {
-    // TODO connect to openai api in gpt folder
-    res.send(req.params.qs);
+router.get('/question=:qs', requestLimter, async (req, res) => {
+    let prompt = await createPrompt(req.params.qs);
+    let openAIRes = await openaiApi(prompt);
+
+    console.log(openAIRes);
+    // let resJson = JSON.parse(openAIRes.data.choices[0].text.replace("```json", "").replace("```", ""));
+    res.send(openAIRes.data.choices[0].text);
 });
 
 export { router };
