e5d87e95e2b0f4f09f3b281302c51536dbd27185
Guuzzeji <5lh8dl440f@gmail.com>
2023-04-09 00:50:48 -0700

    Editing how api with openai works

--- GPT/openai-api.js
+++ GPT/openai-api.js
@@ -1,11 +1,12 @@
 import * as dotenv from 'dotenv';
 import { Configuration, OpenAIApi } from "openai";
 
-dotenv.config({ path: "../.env" });
+dotenv.config({ path: "./.env" });
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
+
 const modelType = process.env.OPENAI_API_MODEL;
 
 const openai = new OpenAIApi(configuration);
@@ -15,14 +16,10 @@ export default async function (promptStr) {
     const completion = await openai.createCompletion({
         "model": modelType,
         "prompt": promptStr,
-        "max_tokens": 300,
-        "temperature": 0,
-        "top_p": 1,
-        "n": 1,
-        "stream": true,
-        "logprobs": null,
-        "stop": "\n"
-    }, { responseType: "stream" });
+        "max_tokens": parseInt(process.env.OPENAI_API_MAX_TOKENS),
+        "temperature": 0.7,
+        "stream": false,
+    });
 
     return completion;
 }
