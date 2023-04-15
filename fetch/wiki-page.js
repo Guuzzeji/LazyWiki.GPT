deec8d898f7471e41205aa6b40b0f99635f40062
Guuzzeji <5lh8dl440f@gmail.com>
2023-04-14 21:23:08 -0700

    Switching to a full text base (no json) for embedding.

--- fetch/wiki-page.js
+++ fetch/wiki-page.js
@@ -7,46 +7,19 @@ import { JSDOM } from 'jsdom';
 async function getWikiPage(pageTitle) {
     let urlTitle = encodeURIComponent(pageTitle);
 
-    let pageData = await fetch(`https://en.wikipedia.org/api/rest_v1/page/mobile-sections/${urlTitle}`)
+    let pageData = await fetch(`https://en.wikipedia.org/api/rest_v1/page/html/${urlTitle}`)
         .then((response) => {
-            return response.json();
+            return response.text();
         }).then((data) => {
             return data;
         });
 
-    let sections = pageData.remaining.sections;
-    sections.unshift({
-        id: 0,
-        line: "Overview",
-        text: pageData.lead.sections[0].text
-    });
-
-    for (let i = 0; i < sections.length; i++) {
-        sections[i].text = htmlToText(sections[i].text);
-    }
-
-    return {
-        title: pageData.lead.normalizedtitle,
-        sections
-    };
+    return htmlToText(pageData);
 };
 
 function htmlToText(html) {
-    let fullText = "";
-
-    let par = html.split("\n");
-
-    for (let i = 0; i < par.length; i++) {
-        let dom = new JSDOM(par[i]).window.document;
-
-        // ! Note to self: Readability will not return a empty string, instead return null
-        if (isProbablyReaderable(dom)) {
-            let text = new Readability(dom).parse();
-            fullText += "\n" + text.textContent;
-        }
-    }
-
-    return fullText.trim();
+    let dom = new JSDOM(html).window.document;
+    return new Readability(dom).parse().textContent.trim();
 };
 
 export { getWikiPage };
