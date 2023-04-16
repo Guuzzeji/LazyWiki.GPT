c98d410bba2171bd23668bdf98b5d4c446925b76
Guuzzeji <5lh8dl440f@gmail.com>
2023-04-16 00:36:11 -0700

    Creating func for embedding
    Note: testing methods are here, cache it to not waste money on embedding the same wiki page.

--- GPT/embedding.js
+++ GPT/embedding.js
@@ -1,9 +1,9 @@
 import * as dotenv from 'dotenv';
 import { Configuration, OpenAIApi } from "openai";
 
-import { chunkText, checkTextSize } from './token';
+import { chunkText, checkTextSize } from './token.js';
 
-dotenv.config({ path: "./.env" });
+dotenv.config({ path: "../.env" });
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
@@ -11,24 +11,89 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
+async function createTextEmbedded(wikipage) {
+    let embed = [];
 
-function combineJSONText(json) {
-    let result = "";
+    for (let x = 0; x < wikipage.sections.length - 5; x++) {
+        let section = wikipage.sections[x];
 
-    result += json.title + "\n";
+        let vectors = [];
 
-    for (let text in json.sections) {
-        result += text + "\n";
+        for (let y = 0; y < section.text.length; y++) {
+            let result = await openai.createEmbedding({
+                input: section.text[y],
+                model: "text-embedding-ada-002"
+            });
+
+            vectors.push(result.data.data[0].embedding);
+        }
+
+        embed.push({
+            title: section.line,
+            vectors: vectors
+        });
     }
 
-    return result;
+    return embed;
 }
 
-async function createTextEmbedded(text) {
+async function searchEmbedding(search) {
     let result = await openai.createEmbedding({
-        input: text.replaceAll("/n", " "),
+        input: search,
         model: "text-embedding-ada-002"
     });
 
-    result.data[0].embedding;
+    return result.data.data[0].embedding;
 }
+
+// Testing
+import { getWikiPage } from '../fetch/wiki-page.js';
+import fs from "fs";
+import cosineSimilarity from "compute-cosine-similarity";
+
+async function main() {
+    // let page = await getWikiPage("Apple inc.");
+
+    // console.log(page.sections[0].text.length);
+
+    // fs.writeFileSync("./wikipage.json", JSON.stringify(page));
+
+    let page = fs.readFileSync("./wikipage.json");
+    page = JSON.parse(page);
+
+    // let emebeding = await createTextEmbedded(page);
+
+    // let jsonWiki = { embedding: emebeding };
+
+    // fs.writeFileSync("./cacheData.json", JSON.stringify(jsonWiki));
+
+    let result = await searchEmbedding("Who was the CEO in 1980");
+
+    let jsonSearch = { embedding: result };
+
+    fs.writeFileSync("./searchCacheData.json", JSON.stringify(jsonSearch));
+
+    let data = fs.readFileSync("./cacheData.json");
+    let search = fs.readFileSync("./searchCacheData.json");
+    data = JSON.parse(data);
+    search = JSON.parse(search);
+
+    let max = { titleIndex: 0, indexVector: 0, similarity: 0 };
+    for (let x = 0; x < data.embedding.length; x++) {
+        let embedding = data.embedding[x];
+        for (let y = 0; y < embedding.vectors.length; y++) {
+            let similarity = cosineSimilarity(embedding.vectors[y], search.embedding);
+            if (max.similarity < similarity) {
+                max.titleIndex = x;
+                max.indexVector = y;
+                max.similarity = similarity;
+            }
+        }
+    }
+
+    console.log(max);
+    console.log(page.sections[max.titleIndex].line);
+    console.log(page.sections[max.titleIndex].text[max.indexVector]);
+}
+
+main();
