44b82a2965c11ab36092d49c4f85d365130f2d36
Guuzzeji <5lh8dl440f@gmail.com>
2023-04-22 00:56:41 -0700

    Refactor func to work with api now

--- GPT/embedding.js
+++ GPT/embedding.js
@@ -1,7 +1,6 @@
 import * as dotenv from 'dotenv';
 import { Configuration, OpenAIApi } from "openai";
-
-import { chunkText, checkTextSize } from './token.js';
+import cosineSimilarity from "compute-cosine-similarity";
 
 dotenv.config({ path: "../.env" });
 
@@ -11,89 +10,51 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-async function createTextEmbedded(wikipage) {
-    let embed = [];
-
-    for (let x = 0; x < wikipage.sections.length - 5; x++) {
-        let section = wikipage.sections[x];
+async function createTextEmbedding(wikipage) {
+    for (let x = 0; x < wikipage.length; x++) {
+        let section = wikipage[x];
 
         let vectors = [];
 
-        for (let y = 0; y < section.text.length; y++) {
+        for (let y = 0; y < section.tokenText.length; y++) {
             let result = await openai.createEmbedding({
-                input: section.text[y],
+                input: section.tokenText[y],
                 model: "text-embedding-ada-002"
             });
 
             vectors.push(result.data.data[0].embedding);
         }
 
-        embed.push({
-            title: section.line,
-            vectors: vectors
-        });
+        section["embedding"] = vectors;
     }
 
-    return embed;
+    return wikipage;
 }
 
-async function searchEmbedding(search) {
+async function searchEmbedding(search, wikipage) {
     let result = await openai.createEmbedding({
         input: search,
         model: "text-embedding-ada-002"
     });
 
-    return result.data.data[0].embedding;
-}
+    let searchQuery = result.data.data[0].embedding;
 
-// Testing
-import { getWikiPage } from '../fetch/wiki-page.js';
-import fs from "fs";
-import cosineSimilarity from "compute-cosine-similarity";
+    let maxPercent = { page: wikipage[0], similarity: 0 };
 
-async function main() {
-    // let page = await getWikiPage("Apple inc.");
+    for (let x = 0; x < wikipage.length; x++) {
+        let page = wikipage[x];
 
-    // console.log(page.sections[0].text.length);
+        for (let y = 0; y < page.embedding.length; y++) {
+            let similarity = cosineSimilarity(page.embedding[y], searchQuery);
 
-    // fs.writeFileSync("./wikipage.json", JSON.stringify(page));
-
-    let page = fs.readFileSync("./wikipage.json");
-    page = JSON.parse(page);
-
-    // let emebeding = await createTextEmbedded(page);
-
-    // let jsonWiki = { embedding: emebeding };
-
-    // fs.writeFileSync("./cacheData.json", JSON.stringify(jsonWiki));
-
-    let result = await searchEmbedding("Who was the CEO in 1980");
-
-    let jsonSearch = { embedding: result };
-
-    fs.writeFileSync("./searchCacheData.json", JSON.stringify(jsonSearch));
-
-    let data = fs.readFileSync("./cacheData.json");
-    let search = fs.readFileSync("./searchCacheData.json");
-    data = JSON.parse(data);
-    search = JSON.parse(search);
-
-    let max = { titleIndex: 0, indexVector: 0, similarity: 0 };
-    for (let x = 0; x < data.embedding.length; x++) {
-        let embedding = data.embedding[x];
-        for (let y = 0; y < embedding.vectors.length; y++) {
-            let similarity = cosineSimilarity(embedding.vectors[y], search.embedding);
-            if (max.similarity < similarity) {
-                max.titleIndex = x;
-                max.indexVector = y;
-                max.similarity = similarity;
+            if (maxPercent.similarity < similarity) {
+                maxPercent.page = page;
+                maxPercent.similarity = similarity;
             }
         }
     }
 
-    console.log(max);
-    console.log(page.sections[max.titleIndex].line);
-    console.log(page.sections[max.titleIndex].text[max.indexVector]);
+    return maxPercent;
 }
 
-main();
+export { searchEmbedding, createTextEmbedding };
