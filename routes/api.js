4028b9d30d81e9e1d2ad6a0f72d92d74c1828000
Guuzzeji <5lh8dl440f@gmail.com>
2023-04-22 01:35:22 -0700

    Updating endpoints and search code

--- routes/api.js
+++ routes/api.js
@@ -2,14 +2,17 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 import { createGeneralQS, createContextQS } from '../GPT/prompt.js';
+import { createTextEmbedding, searchEmbedding } from '../GPT/embedding.js';
 import openaiApi from '../GPT/openai-api.js';
+
+import { getWikiPage } from '../fetch/wiki-page.js';
 
 import requestLimter from './request-limiter.js';
 
 const router = express.Router();
 router.use(bodyParser.json());
 
-router.post('/general_question', requestLimter, async (req, res) => {
+router.post('/general-question', requestLimter, async (req, res) => {
     let jsonReq = req.body;
     let prompt = await createGeneralQS(jsonReq.question);
     let openAIRes = await openaiApi(prompt);
@@ -26,14 +29,14 @@ router.post('/general_question', requestLimter, async (req, res) => {
     }
 });
 
-router.post('/wikipage_question', requestLimter, async (req, res) => {
+router.post('/wikipage-question', requestLimter, async (req, res) => {
     let jsonReq = req.body;
 
     let page = await getWikiPage(jsonReq.wikiTitle);
     page.sections = await createTextEmbedding(page.sections);
     let wikiText = await searchEmbedding(jsonReq.question, page.sections);
 
-    let prompt = await createContextQS(jsonReq.question, wikiText);
+    let prompt = await createContextQS(jsonReq.question, wikiText.page.tokenText[wikiText.embeddingIndex]);
     let openAIRes = await openaiApi(prompt);
 
     console.log(prompt);
