d924433a2e16c402dce59204189580553a85ef7f
Guuzzeji <5lh8dl440f@gmail.com>
2023-04-22 01:36:06 -0700

    Remove old GPT model code

--- GPT/openai-api.js
+++ GPT/openai-api.js
@@ -14,15 +14,6 @@ const openai = new OpenAIApi(configuration);
 // Look at https://github.com/openai/openai-node/issues/107
 export default async function (promptStr) {
 
-    // == Using old gpt-3.5 model - cost more ==
-    // const completion = await openai.createCompletion({
-    //     "model": modelType,
-    //     "prompt": promptStr,
-    //     "max_tokens": parseInt(process.env.OPENAI_API_MAX_TOKENS),
-    //     "temperature": 0.7,
-    //     "stream": false,
-    // });
-
     const completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
         messages: [{ role: "user", content: promptStr }],
