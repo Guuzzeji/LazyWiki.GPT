fc05cb80202a47338f320bf3df6c2c9f30ec3d8a
Guuzzeji <5lh8dl440f@gmail.com>
2023-04-22 01:37:29 -0700

    Update rate limiter to be half a minute

--- routes/request-limiter.js
+++ routes/request-limiter.js
@@ -3,7 +3,7 @@ const requests = {};
 export default function requestLimter(req, res, next) {
     const ip = req.ip;
     const now = Date.now();
-    const timeFrame = 5 * 60 * 1000; // 5 minutes
+    const timeFrame = 0.5 * 60 * 1000; // 5 minutes
 
     if (requests[ip] && requests[ip] > now - timeFrame) {
         return res.status(429).json({ message: 'Too many requests' });
