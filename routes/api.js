15dce5791e59f2e636fe2d4eff07000df53794c3
Guuzzeji <5lh8dl440f@gmail.com>
2023-04-22 00:17:05 -0700

    Relabeling endpoints for api

--- routes/api.js
+++ routes/api.js
@@ -9,7 +9,24 @@ import requestLimter from './request-limiter.js';
 const router = express.Router();
 router.use(bodyParser.json());
 
-router.post('/question', requestLimter, async (req, res) => {
+router.post('/general_question', requestLimter, async (req, res) => {
+    let jsonReq = req.body;
+    let prompt = await createPrompt(jsonReq.question);
+    let openAIRes = await openaiApi(prompt);
+
+    console.log(prompt);
+    console.log(openAIRes.data);
+
+    // Try to parse json from gpt
+    try {
+        let resJson = JSON.parse(openAIRes.data.choices[0].message.content.replace("```json", "").replace("```", ""));
+        res.send(resJson);
+    } catch {
+        res.send(openAIRes.data.choices[0].message.content);
+    }
+});
+
+router.post('/wikipage_question', requestLimter, async (req, res) => {
     let jsonReq = req.body;
     let prompt = await createPrompt(jsonReq.question);
     let openAIRes = await openaiApi(prompt);
