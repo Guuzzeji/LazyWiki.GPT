4d1f173fb9b51057f7e2423f58bd8037a205e437
Guuzzeji <5lh8dl440f@gmail.com>
2023-04-29 00:01:09 -0700

    Editing prompts to prefix for question and context

--- OpenAI/prompts/contextbase.js
+++ OpenAI/prompts/contextbase.js
@@ -3,11 +3,11 @@ import { StructuredOutputParser } from "langchain/output_parsers";
 
 //! Answer user question with more context
 const answer = StructuredOutputParser.fromNamesAndDescriptions({
-    answers: `Your answer to the user question. Use Markdown text to help answer the question. If you cannot answer the user questions write "I don't know" and explain why you cannot answer the question.`,
+    answers: `Your answer to the user question. Your answer should be between 200 to 750 words. If you cannot answer the user questions write "I don't know" and explain why you cannot answer the question.`,
 });
 
 const prompt = new PromptTemplate({
-    template: `AI GPT base on the context given to you and your knowledge of the world, answer the user questions as best as possible. \n User question: {question} \n Context: {context} \n {format_instructions}`,
+    template: `\nContext: {context} \nUser question: {question} \nAI GPT base on the context given to you and your knowledge of the world, answer the user questions. If are asked to make a choice, make an arugment for every choice offered by the user. \n{format_instructions}`,
     inputVariables: ["question", "context"],
     partialVariables: { format_instructions: answer.getFormatInstructions() },
 });
--- OpenAI/prompts/search.js
+++ OpenAI/prompts/search.js
@@ -5,14 +5,13 @@ import searchWiki from "../../fetch/wiki-search.js";
 
 //! Answer general user question
 const answer = StructuredOutputParser.fromNamesAndDescriptions({
-    answers: `Provide answer with source citation in this format: some text[^1](website url). Don't Markdown to answer the question. Answer limit is 500 words. Write "I don't know" if you are unable to answer, along with a reason why you cannot answer the question. Avoid using double quotes in answer.`,
-    best: "Suggest the most probable website URL from the provided list to answer the user's question.",
-    sources: ["Provide a list of website URLs from the given to you list that were used to answer the user's question in the order of citations."],
-    listBest: ["Select websites that can futher answer the user's question, select 2 to 4 websites and place them here."]
+    answers: `Provide answer with source citation in this format: some text[^1](website url). Answer limit is 500 words. Write "I don't know" if you are unable to answer, along with a reason why you cannot answer the question. Avoid using double quotes in answer.`,
+    sources: ["Provide a list of website URLs from the given to you list that were used to answer the user's question in the order of citations"],
+    listBest: ["Select websites out of the list given to you that can futher answer the user's question. Select 1 to 4 websites and place them here."]
 });
 
 const prompt = new PromptTemplate({
-    template: `AI GPT answer the user's question using given websites only. Use the most useful websites provided and write answers in JSON format. \n User question: {question} \n Websites: {websites} \n {format_instructions}`,
+    template: `\nUser question:{question} \nWebsites:{websites}\n AI GPT answer the user's question using given websites only. Never use websites outside the websites provide to you in the list. Use the most useful websites provided and write answers in JSON format. {format_instructions}`,
     inputVariables: ["question", "websites"],
     partialVariables: { format_instructions: answer.getFormatInstructions() },
 });
