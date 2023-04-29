d45c0e5a919910391111e74f1fd8b78d05dc641c
Guuzzeji <5lh8dl440f@gmail.com>
2023-04-28 23:38:07 -0700

    Editing prompt

--- OpenAI/prompts/contextbase.js
+++ OpenAI/prompts/contextbase.js
@@ -3,11 +3,11 @@ import { StructuredOutputParser } from "langchain/output_parsers";
 
 //! Answer user question with more context
 const answer = StructuredOutputParser.fromNamesAndDescriptions({
-    answers: `Your answer to the user question. You can use Markdown to answer the question. If you cannot answer the user questions wirte "I don't know" and explain why you cannot answer the question.`,
+    answers: `Your answer to the user question. Use Markdown text to help answer the question. If you cannot answer the user questions write "I don't know" and explain why you cannot answer the question.`,
 });
 
 const prompt = new PromptTemplate({
-    template: `AI GPT base on the context given to you and your knowledge of the world, answer the user questions. \n User question: {question} \n Context: {context} \n {format_instructions}`,
+    template: `AI GPT base on the context given to you and your knowledge of the world, answer the user questions as best as possible. \n User question: {question} \n Context: {context} \n {format_instructions}`,
     inputVariables: ["question", "context"],
     partialVariables: { format_instructions: answer.getFormatInstructions() },
 });
