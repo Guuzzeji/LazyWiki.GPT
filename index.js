c30b9736b9500b39fa6919dd0cdecd5c3156e55a
Guuzzeji <5lh8dl440f@gmail.com>
2023-04-29 21:43:53 -0700

    Adding a way for main server to host react client

--- index.js
+++ index.js
@@ -11,8 +11,13 @@ const port = process.env.PORT || 3000;
 app.use(morgan('tiny'));
 app.use("/API", api);
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+// Used for react client
+// from: https://levelup.gitconnected.com/how-to-render-react-app-using-express-server-in-node-js-a428ec4dfe2b
+// also helpful: https://stackoverflow.com/questions/40262513/how-to-disable-suppress-errors-from-libraries-in-typescript
+app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static("public"));
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
 
 app.listen(port, () => {
