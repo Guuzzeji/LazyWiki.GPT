ad1380f3d39250f22f2e21752ee235454cbd575b
Guuzzeji <5lh8dl440f@gmail.com>
2023-05-16 10:29:07 -0700

    Creating answer component

---
+++ public/client/src/components/Answer.tsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import { Button, Input, Space } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+
+
+export interface General {
+    answers: string
+    sources: string[]
+    listBest: string[]
+}
+
+export function GeneralAnswer(answer: General) {
+
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export function ContextAnswer() {
+
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+
