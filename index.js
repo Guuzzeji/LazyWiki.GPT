8713cde119c24083d59136fd1edf886f5ac59fa1
Guuzzeji <5lh8dl440f@gmail.com>
2023-05-16 12:08:03 -0700

    Adding client to backend and creating build scripts

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
 import morgan from 'morgan';
+import path from 'path';
 
 import { router as api } from './routes/api.js';
 
@@ -14,10 +15,11 @@ app.use("/API", api);
 // Used for react client
 // from: https://levelup.gitconnected.com/how-to-render-react-app-using-express-server-in-node-js-a428ec4dfe2b
 // also helpful: https://stackoverflow.com/questions/40262513/how-to-disable-suppress-errors-from-libraries-in-typescript
-app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static(path.join(process.cwd(), "public/client/", "build")));
 app.use(express.static("public"));
 app.use((req, res, next) => {
-    res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+    console.log(path.join(process.cwd(), "public/client/build", "index.html"));
+    res.sendFile(path.join(process.cwd(), "public/client/build", "index.html"));
 });
 
 app.listen(port, () => {
