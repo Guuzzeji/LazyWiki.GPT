1185c129e3cafdf2e03a54d731ea9ffd3f6dab04
Guuzzeji <5lh8dl440f@gmail.com>
2023-05-16 11:01:36 -0700

    Creating basic created for general answers search

--- public/client/src/components/Answer.tsx
+++ public/client/src/components/Answer.tsx
@@ -1,20 +1,52 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Input, Space } from 'antd';
+import { Button, Card, Space } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
 
 
 export interface General {
-    answers: string
+    answer: string
     sources: string[]
     listBest: string[]
 }
 
+function SourceItem(url: string) {
+    return (
+        <li>
+            <a href={url}>url</a>
+        </li>
+    )
+}
+
 export function GeneralAnswer(answer: General) {
 
-    return (
-        <div>
+    let sources = [];
+    for (let i = 0; i < answer.sources.length; i++) {
+        sources.push(SourceItem(answer.sources[i]))
+    }
 
-        </div>
+    let bestSources = [];
+    for (let i = 0; i < answer.listBest.length; i++) {
+        bestSources.push(SourceItem(answer.listBest[i]))
+    }
+
+    return (
+        <Card>
+            <p>{answer.answer}</p>
+
+            <p><u>Futher Your Answer</u></p>
+            <ol>
+                {bestSources}
+            </ol>
+
+            <p><u>Sources</u></p>
+            <ol>
+                {sources}
+            </ol>
+
+            <center>
+                <Button type="dashed">Context Answer</Button>
+            </center>
+        </Card>
     );
 }
 
